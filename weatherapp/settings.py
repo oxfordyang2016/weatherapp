8ab2a314ca4d896fc0b365c88cabbe1b91ce8389
yangming <yang756260386@gmail.com>
2016-07-10 15:09:52 +0800

    add procfile, changed staic

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fs2@y$$5pkzwjlt7j7lu1&8am9ecrka*2+u@#43qf3_)^iwn-$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [yangmingweatherapp.heroku.com]
 
 
 # Application definition
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path .join(BASE_DIR ,'static'),
 ]
+STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT =os.path.join(BASE_DIR,'static')
+
--- weatherapp/wsgi.py
+++ weatherapp/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherapp.settings")
+from whitenoise.django import DjangoWhiteNoise
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weat herapp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
