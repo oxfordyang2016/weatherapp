4a4bfe77af8209002eb76a273995f62075f83509
yangming <yang756260386@gmail.com>
2016-07-10 15:20:45 +0800

    ''

--- weatherapp/wsgi.py
+++ weatherapp/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weat herapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherapp.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
