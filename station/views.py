88487df3d81b02cc08f179da170cca1cfb7baa22
yangming <yang756260386@gmail.com>
2016-07-11 13:16:46 +0800

    ''

--- station/views.py
+++ station/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import render_to_response
 from django.http import HttpRequest
 from django.http import HttpResponse
 from datetime import datetime
+import requests
 #from django.tmplate.response import TempalteResponse
 from station.models import Reading
 from ipware.ip import get_ip
 ip_pool = []
-
+#as visitor count
 def home(request):
     dat = Reading.objects.last()
     ti = datetime.now()
@@ -17,10 +18,13 @@ def home(request):
     #context['data']=dat
     #return HttpResponse(request,'index.html',context)
     ip = get_ip(request)
+    url = 'http: // ip - api.com / json /'+str(ip)
+    r = requests.get(url).json()
     ip_pool.append(ip)
-    visitor_people = 'You Are The'+str(len(ip_pool)) +'st'+'Visitor'
+
+    visitor_people = 'You Are The'+str(len(ip_pool)) +'st  '+'Visitor'
     if ip is not None:
         print("we have an IP address for user")
     else:
         print("we don't have an IP address for user")
-    return render_to_response('index.html', {'data': ti,'weather':dat ,'visitor_ip':ip,'count_visitor':visitor_people})
+    return render_to_response('index.html', {'data': ti,'weather':dat ,'visitor_ip':ip,'count_visitor':visitor_people,'more_info':r})
